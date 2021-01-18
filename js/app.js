a91ae61d950e980fd127716a6b5b1b43c5ac9a83
Alex Andreev <webme.corporate@gmail.com>
2021-01-19 04:48:06 +1100

    fix bugs

--- js/app.js
+++ js/app.js
@@ -2,29 +2,38 @@
 let modal = document.querySelector('.modal');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+let userEditBtn = document.querySelector('.profile__edit-btn');
+let formCloseBtn = document.querySelector('.modal__close-btn');
+let formElement = document.querySelector('.modal__container');
+let nameInput = document.querySelector('.modal__input_type_username');
+let aboutInput = document.querySelector('.modal__input_type_about');
 
 // Показать модальное окно
-let userEditBtn = document.querySelector('.profile__edit-btn');
-userEditBtn.addEventListener('click', function () {
+function showModal() {
 	modal.classList.add('modal_opened');
-});
+	nameInput.setAttribute('value', profileName.textContent);
+	aboutInput.setAttribute('value', profileAbout.textContent);
+}
 
 // Закрыть модальное окно
-let formCloseBtn = document.querySelector('.modal__close-btn');
-formCloseBtn.addEventListener('click', function () {
-	modal.classList.remove('modal_opened');
-});
-
-// Редактировать профиль и сохранить результат
-let formElement = document.querySelector('.modal__container');
-let nameInput = document.querySelector('#userName');
-let aboutInput = document.querySelector('#userAbout');
-
-function formSubmitHandler(save) {
-	save.preventDefault();
-	profileName.textContent = nameInput.value;
-	profileAbout.textContent = aboutInput.value;
+function closeModal() {
 	modal.classList.remove('modal_opened');
 }
+
+// Редактировать профиль и сохранить результат
+function formSubmitHandler(event) {
+	event.preventDefault();
+	profileName.textContent = nameInput.value;
+	profileAbout.textContent = aboutInput.value;
+	closeModal();
+}
+
+userEditBtn.addEventListener('click', function () {
+	showModal();
+});
+
+formCloseBtn.addEventListener('click', function () {
+	closeModal();
+});
 
 formElement.addEventListener('submit', formSubmitHandler);
