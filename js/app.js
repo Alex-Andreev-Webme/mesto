322984781acb179cdf8cf3d61c107d942afa95bc
Alex Andreev <webme.corporate@gmail.com>
2021-02-04 05:21:14 +0300

    ref scripts

--- js/app.js
+++ js/app.js
@@ -7,11 +7,12 @@ const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 const userEditBtn = document.querySelector('.profile__edit-btn');
 
+nameInput.value = profileName.textContent;
+aboutInput.value = profileAbout.textContent;
+
 // Показать модальное окно
 function showModal() {
 	modal.classList.add('modal_opened');
-	nameInput.value = profileName.textContent;
-	aboutInput.value = profileAbout.textContent;
 }
 
 // Закрыть модальное окно
@@ -19,7 +20,7 @@ function closeModal() {
 	modal.classList.remove('modal_opened');
 }
 
-// Редактировать профиль и сохранить результат
+// Обработчик формы
 function formSubmitHandler(event) {
 	event.preventDefault();
 	profileName.textContent = nameInput.value;
@@ -28,14 +29,10 @@ function formSubmitHandler(event) {
 }
 
 // Показать модальное окно при клике
-userEditBtn.addEventListener('click', function () {
-	showModal();
-});
+userEditBtn.addEventListener('click', showModal);
 
 // Скрыть модальное окно при клике
-modalCloseBtn.addEventListener('click', function () {
-	closeModal();
-});
+modalCloseBtn.addEventListener('click', closeModal);
 
 // Редактировать профиль и сохранить результат при клике
 formElement.addEventListener('submit', formSubmitHandler);
