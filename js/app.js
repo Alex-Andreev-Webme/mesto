5716edc1936f0cb66ff2d0abcef973957da8dc0f
Alex Andreev <webme.corporate@gmail.com>
2021-02-10 08:42:22 +0300

    feat: add like and remove

--- js/app.js
+++ js/app.js
@@ -76,25 +76,28 @@ const initialCards = [
 const cardsContainer = document.querySelector('.cards'); // родительский блок для карточек
 const cardsTemplate = document.querySelector('.cards-template').content; // содержание тега template
 
-function redrerCards() {
-	const initialCardsItem = initialCards.map(addCards);
-	cardsContainer.append(...initialCardsItem);
-}
-
-redrerCards();
-
-// Добавить карточки из массива на страницу
-function addCards(card) {
+// --- Шесть карточек «из коробки» --- //
+function getCards(card) {
 	const cardElement = cardsTemplate.cloneNode(true);
 	const cardImage = cardElement.querySelector('.card__image');
 	const cardTitle = cardElement.querySelector('.card__title');
 	cardImage.src = card.src;
 	cardImage.alt = card.alt;
 	cardTitle.textContent = card.title;
+	const removeBtn = cardElement.querySelector('.card__remove-btn');
+	const likeBtn = cardElement.querySelector('.card__like-btn');;
+	removeBtn.addEventListener('click', removeCard);
+	likeBtn.addEventListener('click', likeCard);
 	return cardElement;
 }
 
-// Форма добавления карточки
+function redrerCards() {
+	const html = initialCards.map(getCards);
+	cardsContainer.append(...html);
+}
+
+redrerCards();
+
 const AddPhotoBtn = document.querySelector('.profile__add-btn');
 const photoPopup = document.querySelector('.photo-popup');
 const photoPopupCloseBtn = document.querySelector('.photo-popup__close-btn');
@@ -109,13 +112,13 @@ function closePhotoPopup() {
 }
 
 // Добавить и сохранить новую карточку на страницу
-function addNewCardHandler(event) {
+function addCardHandler(event) {
 	event.preventDefault();
 	const photoInputLink = document.querySelector('.photo-popup__input_type_link');
 	const photoInputTitle = document.querySelector('.photo-popup__input_type_title');
 	const inputLinkValue = photoInputLink.value;
 	const inputTitleValue = photoInputTitle.value;
-	const cardItem = addCards({ src: inputLinkValue, title: inputTitleValue });
+	const cardItem = getCards({ src: inputLinkValue, title: inputTitleValue });
 	cardsContainer.prepend(cardItem);
 	photoInputLink.value = '';
 	photoInputTitle.value = '';
@@ -123,33 +126,28 @@ function addNewCardHandler(event) {
 }
 
 // Поставить лайк
-const likeBtn = document.querySelectorAll('.card__like-btn');
+function likeCard(event) {
+	const targetEl = event.target;
+	const targetItem = targetEl.classList.toggle('card__like-btn_active');
+}
 
-likeBtn.forEach(function (like) {
-	like.addEventListener('click', () => {
-		like.classList.toggle('.card__like-btn_active'); // если обратиться к likeBtn, то появляется ошибка
-		console.log('clicked');
-	});
-	console.log(like);
-});
+// Удалить карточку
+function removeCard(event) {
+	const targetEl = event.target;
+	const targetItem = targetEl.closest('.card');
+	targetItem.remove();
+}
 
-// Почему при попытке повесить обработчик события появляется ошибка undefined?
-// likeBtn.addEventListener('click', likeToggle); // изначально функция так называлась
+// const likeBtn = document.querySelectorAll('.card__like-btn');
 
-// Более короткая запись через стрелочную функцию, 
-// но я не понимаю, как ее вызвать в обработчике события, разобрался, уже не актуально
-// likeBtn.forEach((like) => { like.classList.toggle('.card__like-btn_active') && console.log(like) });
-
-// Такой метод решения предлагают в Интернете, тоже не пашет
-// likeBtn.forEach((like) => {
-// 	like.addEventListener('click', function () {
-// 		likeBtn.classList.toggle('.card__like-btn_active');
+// likeBtn.forEach(function (item) {
+// 	item.addEventListener('click', function () {
+// 		item.classList.toggle('card__like-btn_active');
 // 	});
 // });
-
 
 AddPhotoBtn.addEventListener('click', showPhotoPopup);
 
 photoPopupCloseBtn.addEventListener('click', closePhotoPopup);
 
-photoPopupForm.addEventListener('submit', addNewCardHandler);
+photoPopupForm.addEventListener('submit', addCardHandler);
