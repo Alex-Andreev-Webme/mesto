387387a1af62450549988d6c1189c42209177fce
Alex Andreev <webme.corporate@gmail.com>
2021-02-10 05:34:15 +0300

    feat: add card

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ const profilePopupAboutInput = document.querySelector('.profile-popup__input_typ
 const userName = document.querySelector('.profile__name');
 const userAbout = document.querySelector('.profile__about');
 
-profilePopupNameInput.value = userName.textContent;
-profilePopupAboutInput.value = userAbout.textContent;
 
 // Показать попап редактирования профиля
 function showProfilePopup() {
 	profilePopup.classList.add('profile-popup_opened');
+	profilePopupNameInput.value = userName.textContent;
+	profilePopupAboutInput.value = userAbout.textContent;
 }
 
 // Закрыть попап редактирования профиля
@@ -73,27 +73,32 @@ const initialCards = [
 	},
 ];
 
+const cardsContainer = document.querySelector('.cards'); // родительский блок для карточек
+const cardsTemplate = document.querySelector('.cards-template').content; // содержание тега template
+
+function redrerCards() {
+	const initialCardsItem = initialCards.map(addCards);
+	cardsContainer.append(...initialCardsItem);
+}
+
+redrerCards();
+
+// Добавить карточки из массива на страницу
+function addCards(card) {
+	const cardElement = cardsTemplate.cloneNode(true);
+	const cardImage = cardElement.querySelector('.card__image');
+	const cardTitle = cardElement.querySelector('.card__title');
+	cardImage.src = card.src;
+	cardImage.alt = card.alt;
+	cardTitle.textContent = card.title;
+	return cardElement;
+}
+
+// Форма добавления карточки
 const AddPhotoBtn = document.querySelector('.profile__add-btn');
 const photoPopup = document.querySelector('.photo-popup');
 const photoPopupCloseBtn = document.querySelector('.photo-popup__close-btn');
 const photoPopupForm = document.querySelector('.photo-popup__form');
-const cardLikeBtn = document.querySelectorAll('.card__like-btn');
-const cardsContainer = document.querySelector('.cards');
-const cardsTemplate = document.querySelector('.cards-template').content;
-
-function renderCards(card) {
-	const cardElement = cardsTemplate.cloneNode(true);
-	console.log(cardElement);
-	cardElement.querySelector('.card__title').textContent = 'Рускеала';
-	cardElement.querySelector('.card__image').src = './images/card-ruskeala.jpg';
-	// Нужно понять, как подтянуть данные из всех 6 карточек из массива
-	// cardElement.querySelector('.card__image').src = card.src;
-	// cardElement.querySelector('.card__image').alt = card.alt;
-	// cardElement.querySelector('.card__image').textContent = card.title;
-	cardsContainer.append(cardElement);
-}
-
-renderCards();
 
 function showPhotoPopup() {
 	photoPopup.classList.add('photo-popup_opened');
@@ -103,13 +108,48 @@ function closePhotoPopup() {
 	photoPopup.classList.remove('photo-popup_opened');
 }
 
-function photoSubmitHandler(event) {
+// Добавить и сохранить новую карточку на страницу
+function addNewCardHandler(event) {
 	event.preventDefault();
+	const photoInputLink = document.querySelector('.photo-popup__input_type_link');
+	const photoInputTitle = document.querySelector('.photo-popup__input_type_title');
+	const inputLinkValue = photoInputLink.value;
+	const inputTitleValue = photoInputTitle.value;
+	const cardItem = addCards({ src: inputLinkValue, title: inputTitleValue });
+	cardsContainer.prepend(cardItem);
+	photoInputLink.value = '';
+	photoInputTitle.value = '';
 	closePhotoPopup();
 }
+
+// Поставить лайк
+const likeBtn = document.querySelectorAll('.card__like-btn');
+
+likeBtn.forEach(function (like) {
+	like.addEventListener('click', () => {
+		like.classList.toggle('.card__like-btn_active'); // если обратиться к likeBtn, то появляется ошибка
+		console.log('clicked');
+	});
+	console.log(like);
+});
+
+// Почему при попытке повесить обработчик события появляется ошибка undefined?
+// likeBtn.addEventListener('click', likeToggle); // изначально функция так называлась
+
+// Более короткая запись через стрелочную функцию, 
+// но я не понимаю, как ее вызвать в обработчике события, разобрался, уже не актуально
+// likeBtn.forEach((like) => { like.classList.toggle('.card__like-btn_active') && console.log(like) });
+
+// Такой метод решения предлагают в Интернете, тоже не пашет
+// likeBtn.forEach((like) => {
+// 	like.addEventListener('click', function () {
+// 		likeBtn.classList.toggle('.card__like-btn_active');
+// 	});
+// });
+
 
 AddPhotoBtn.addEventListener('click', showPhotoPopup);
 
 photoPopupCloseBtn.addEventListener('click', closePhotoPopup);
 
-photoPopupForm.addEventListener('submit', photoSubmitHandler);
+photoPopupForm.addEventListener('submit', addNewCardHandler);
