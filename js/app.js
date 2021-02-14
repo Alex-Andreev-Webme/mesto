e576d3ceed8aa778c0ecb4cc7ac7d0424f83524a
Alex Andreev <webme.corporate@gmail.com>
2021-02-14 21:40:24 +0300

    just refactoring

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 const profileEditBtn = document.querySelector('.profile__edit-btn');
-const profilePopupCloseBtn = document.querySelector('.popup__close-btn');
 const profilePopupForm = document.querySelector('.popup__form');
 const profilePopupNameInput = document.querySelector('.popup__input_type_name');
 const profilePopupAboutInput = document.querySelector('.popup__input_type_about');
 const userName = document.querySelector('.profile__name');
 const userAbout = document.querySelector('.profile__about');
-const galleryPopupCloseBtn = document.querySelector('.popup__close-btn_place_gallery');
 const photoInputLink = document.querySelector('.popup__input_type_link');
 const photoInputTitle = document.querySelector('.popup__input_type_title');
 const galleryPopupTitle = document.querySelector('.popup__title_place_gallery');
 const galleryPopupImage = document.querySelector('.popup__image');
 const addPhotoBtn = document.querySelector('.profile__add-btn');
-const photoPopupCloseBtn = document.querySelector('.popup__close-btn_place_photo');
 const photoPopupForm = document.querySelector('.popup__form_place_photo');
 const cardsContainer = document.querySelector('.cards'); // родительский блок для карточек
 const cardsTemplate = document.querySelector('.cards-template').content; // содержание тега template
@@ -20,6 +17,12 @@ const popup = document.querySelector('.popup');
 const profilePopup = document.querySelector('.popup_place_profile');
 const photoPopup = document.querySelector('.popup_place_photo');
 const galleryPopup = document.querySelector('.popup_place_gallery');
+const popupCloseBtns = document.querySelectorAll('.popup__close-btn');
+
+function closePopupClick(event) {
+	const targetPopup = event.target.closest('.popup');
+	closePopup(targetPopup);
+}
 
 function showPopup(popup) {
 	popup.classList.add('popup_opened');
@@ -29,7 +32,7 @@ function closePopup(popup) {
 	popup.classList.remove('popup_opened');
 }
 
-function showProfilePopup() {
+function getProfilePopup() {
 	showPopup(profilePopup);
 	profilePopupNameInput.value = userName.textContent;
 	profilePopupAboutInput.value = userAbout.textContent;
@@ -38,8 +41,8 @@ function showProfilePopup() {
 // Обработчик формы редактирования профиля
 function profileSubmitHandler(event) {
 	event.preventDefault();
-	userName.textContent = profilePopupNameInput.value;
-	userAbout.textContent = profilePopupAboutInput.value;
+	userName.textContent = profilePopupNameInput.value.trim();
+	userAbout.textContent = profilePopupAboutInput.value.trim();
 	closePopup(profilePopup);
 }
 
@@ -66,15 +69,11 @@ function showGalleryPopup(event) {
 	showPopup(galleryPopup);
 }
 
-profileEditBtn.addEventListener('click', showProfilePopup);
-
-profilePopupCloseBtn.addEventListener('click', () => closePopup(profilePopup));
+profileEditBtn.addEventListener('click', getProfilePopup);
 
 addPhotoBtn.addEventListener('click', () => showPopup(photoPopup));
 
-photoPopupCloseBtn.addEventListener('click', () => closePopup(photoPopup));
-
-galleryPopupCloseBtn.addEventListener('click', () => closePopup(galleryPopup));
+popupCloseBtns.forEach(button => button.addEventListener('click', closePopupClick));
 
 // Поставить лайк
 function likeCard(event) {
@@ -83,8 +82,8 @@ function likeCard(event) {
 
 // Удалить карточку
 function removeCard(event) {
-	const targetItem = event.target.closest('.card');
-	targetItem.remove();
+	const targetCard = event.target.closest('.card');
+	targetCard.remove();
 }
 
 photoPopupForm.addEventListener('submit', addCardHandler);
@@ -101,10 +100,9 @@ function getCard(arrItem) {
 	cardTitle.textContent = arrItem.title;
 	const removeBtn = cardElement.querySelector('.card__remove-btn');
 	const likeBtn = cardElement.querySelector('.card__like-btn');
-	const galleryPopupOpenBtn = cardImage;
 	removeBtn.addEventListener('click', removeCard);
 	likeBtn.addEventListener('click', likeCard);
-	galleryPopupOpenBtn.addEventListener('click', showGalleryPopup);
+	cardImage.addEventListener('click', showGalleryPopup);
 	return cardElement;
 }
 
