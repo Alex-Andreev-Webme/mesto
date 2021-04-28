15f1768dec2926e8e2d117afc8d6b6c7c3ebf2cc
Alex Andreev <webme.corporate@gmail.com>
2021-04-28 13:35:16 +0300

    Собираем проект Вебпаком

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ import {
 	profilePopupAboutInput,
 	photoPopupForm,
 	profilePopupForm,
-} from '../utils/constants.js'
+} from './utils/constants.js'
 
-import Card from '../components/Card.js'
-import FormValidator from '../components/FormValidator.js'
-import Section from '../components/Section.js'
-import PopupWithImage from '../components/PopupWithImage.js'
-import PopupWithForm from '../components/PopupWithForm.js'
-import UserInfo from '../components/UserInfo.js'
+import Card from './components/Card.js'
+import FormValidator from './components/FormValidator.js'
+import Section from './components/Section.js'
+import PopupWithImage from './components/PopupWithImage.js'
+import PopupWithForm from './components/PopupWithForm.js'
+import UserInfo from './components/UserInfo.js'
 
 // Добавление карточек из массива на страницу
 const cardList = new Section(
