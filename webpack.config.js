4d540c8d0ce55b535c878ce0df2e2390ff00ef5a
Alex Andreev <webme.corporate@gmail.com>
2021-04-28 16:41:34 +0300

    Настроил бабель

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,15 @@ module.exports = {
 		compress: true,
 		port: 8080,
 		open: true
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: '/node_modules/'
+			}
+		]
 	}
-}
+};
